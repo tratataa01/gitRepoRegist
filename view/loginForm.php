d093ae479747b8612987a21126c5c9fae4cf70c6
Gonchar Artem <traTaTaa01@gmail.com>
2019-07-01 16:42:08 +0300

    .

--- view/loginForm.php
+++ view/loginForm.php
@@ -16,7 +16,7 @@
 
 <button class="btn btn-lg btn-primary btn-block" type="sumit">Войти</button>
     <a href="registration" class="btn btn-lg btn-primary btn-block" type="sumit">Регистрация</a>
-aasdasd
+
 
 </form>
 </div>
